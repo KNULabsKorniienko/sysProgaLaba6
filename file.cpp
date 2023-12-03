d31b067e26dec13dba9f7e01904c42ba3a0f619b
root <root@linux.myguest.virtualbox.org>
2023-12-03 20:48:08 +0200

    first commit

---
+++ file.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Function to calculate factorial
+int calculateFactorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * calculateFactorial(n - 1);
+    }
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a non-negative integer: ";
+    std::cin >> number;
+
+    if (number < 0) {
+        std::cout << "Please enter a non-negative integer." << std::endl;
+        return 1; // Exit with an error code
+    }
+
+    int factorial = calculateFactorial(number);
+
+    std::cout << "Factorial of " << number << " is: " << factorial << std::endl;
+
+    return 0;
